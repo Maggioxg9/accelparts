10fb7f7eeb88b9cf105f81e8b14a32b066e2803d
AccelEntertainment <zacharym@accelentertainment.com>
2017-01-17 01:49:07 -0600

    update

--- res/functions/adminupload.php
+++ res/functions/adminupload.php
@@ -31,23 +31,28 @@
 					$conn = null;
 				}
 		$uploadOk = 1;
-		$target_dir = "../uploads/categories/Bally/";
+		$target_dir = "res/uploads/categories/Bally/";
 		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		if(isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				$uploadOk = 1;
+			} else {
+			$uploadOk = 0;
+			}
+		}
 		if (file_exists($target_file)) {
-			echo "Sorry, file already exists.";
 			$uploadOk = 0;
 		}
 		if ($_FILES["fileToUpload"]["size"] > 4194304) {
-			echo "Sorry, your file is too large.";
 			$uploadOk = 0;
 		}
 		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
 			$uploadOk = 0;
 		}
 		if ($uploadOk == 0) {
-			echo "Sorry, your file was not uploaded.";
+			$_SESSION['uploadsuccess']=false;
 			// if everything is ok, try to upload file
 		} else {
 			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
