a2345001cb8696e7a2f7c3fe92b80d34a046ccb1
AccelEntertainment <zacharym@accelentertainment.com>
2017-01-17 00:43:10 -0600

    update

--- res/functions/adminupload.php
+++ res/functions/adminupload.php
@@ -53,7 +53,7 @@
 			if (move_uploaded_file($_FILES["fileToUpload"]["name"], $target_file)) {
 				echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 			} else {
-				echo $_FILES;
+				echo json_encode($_FILES);
 			}
 		}
 	}
