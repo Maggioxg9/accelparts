ac0b765c0b4aa503e15a03cda7b13a0d175614b7
AccelEntertainment <zacharym@accelentertainment.com>
2017-01-16 21:21:36 -0600

    update

--- res/functions/phpinfo.php
+++ res/functions/phpinfo.php
@@ -1,5 +1,7 @@
 <?php
-echo password_hash("54321", PASSWORD_DEFAULT)."\n";
+echo password_hash("Accel8932", PASSWORD_DEFAULT)."\n";
+echo password_hash("Accel3530", PASSWORD_DEFAULT)."\n";
+echo password_hash("Jabroni3q", PASSWORD_DEFAULT)."\n";
 phpinfo();
 
 ?>
