01feab3b4bd0592930defc4f7cfa0108f015cff7
ZacharyM <Maggioxg9@aol.com>
2017-01-18 13:31:25 -0600

    update

--- res/functions/adminupload.php
+++ res/functions/adminupload.php
@@ -11,42 +11,67 @@
 		$uploadOk = 1;
 		if($_POST['categoryname']=='Form'){
 			$target_dir = "/var/www/html/accelparts/res/uploads/forms/";
+			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+			if(isset($_POST["submit"])) {
+				if ($_FILES["fileToUpload"]["size"] > 4194304) {
+					$uploadOk = 0;
+				}
+				if($imageFileType !="xlsm"){
+					$uploadOk = 0;
+				}
+				if ($uploadOk == 0) {
+					$_SESSION['uploadsuccess']=false;
+					header("Location: ../../adminupload.html");
+					exit();
+					// if everything is ok, try to upload file
+				} else {
+					if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+						$_SESSION['uploadsuccess']=true;
+						header("Location: ../../adminupload.html");
+						exit();
+					} else {
+						$_SESSION['uploadsuccess']=false;
+						header("Location: ../../adminupload.html");
+						exit();
+					}
+				}
+			}
 		}else{
 			$target_dir = "/var/www/html/accelparts/res/uploads/categories/" . $_POST['categoryname'] . "/";
-		}
-		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		if(isset($_POST["submit"])) {
-			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-			if($check !== false) {
-				$uploadOk = 1;
-			} else {
-			$uploadOk = 0;
+			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+			if(isset($_POST["submit"])) {
+				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+				if($check !== false) {
+					$uploadOk = 1;
+				} else {
+				$uploadOk = 0;
+				}
 			}
-		}
-		if (file_exists($target_file)) {
-			$uploadOk = 0;
-		}
-		if ($_FILES["fileToUpload"]["size"] > 4194304) {
-			$uploadOk = 0;
-		}
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType !="xlsm") {
-			$uploadOk = 0;
-		}
-		if ($uploadOk == 0) {
-			$_SESSION['uploadsuccess']=false;
-			header("Location: ../../adminupload.html");
-			exit();
-			// if everything is ok, try to upload file
-		} else {
-			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-				$_SESSION['uploadsuccess']=true;
-				header("Location: ../../adminupload.html");
-				exit();
-			} else {
+			if (file_exists($target_file)) {
+				$uploadOk = 0;
+			}
+			if ($_FILES["fileToUpload"]["size"] > 4194304) {
+				$uploadOk = 0;
+			}
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+				$uploadOk = 0;
+			}
+			if ($uploadOk == 0) {
 				$_SESSION['uploadsuccess']=false;
 				header("Location: ../../adminupload.html");
 				exit();
+				// if everything is ok, try to upload file
+			} else {
+				if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+					$_SESSION['uploadsuccess']=true;
+					header("Location: ../../adminupload.html");
+					exit();
+				} else {
+					$_SESSION['uploadsuccess']=false;
+					header("Location: ../../adminupload.html");
+					exit();
+				}
 			}
 		}
 	}
