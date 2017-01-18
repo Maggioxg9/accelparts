a0053751fb90a6b533da0a4bef52b21d592b3732
ZacharyM <Maggioxg9@aol.com>
2017-01-18 14:48:49 -0600

    update

--- res/functions/adminupload.php
+++ res/functions/adminupload.php
@@ -9,7 +9,7 @@
 	}
 	if(count($_POST) >0){
 		$uploadOk = 1;
-		if($_POST['categoryname']=='Form'){
+		if($_POST['categoryname']=='Form'){ //uploading part request form
 			$target_dir = "/var/www/html/accelparts/res/uploads/forms/";
 			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
@@ -40,7 +40,7 @@
 					}
 				}
 			}
-		}else{
+		}else{ //uploading part image and descriptions
 			$target_dir = "/var/www/html/accelparts/res/uploads/categories/" . $_POST['categoryname'] . "/";
 			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
@@ -68,6 +68,33 @@
 				// if everything is ok, try to upload file
 			} else {
 				if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+					//upload succeeded so add it to the database
+					$servername = "localhost";
+					$username = "root";
+					$password = "accelmkgt123";
+					$dbname = "accelparts";
+					$description = htmlspecialchars($_POST['description']);
+					$accelnumber = htmlspecialchars($_POST['accelnumber']);
+					$categoryname = htmlspecialchars($_POST['categoryname']);
+					$imgpath = $target_file;
+					try{
+						$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+						$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+						
+						$getcategory = $conn-prepare("select categoryid from categories where categoryname= :categoryname");
+						$getcategory ->execute(array(":categoryname" => "$categoryname"));
+						$result = $getcategory->fetch(PDO::FETCH_ASSOC);
+						$categoryidresult = $result['categoryid'];
+						
+						$insertimages = $conn->prepare("insert into parts (description, accelnumber,categoryid, imgpath) values (:description,:accelnumber, :categoryid, :imgpath)");
+						$insertimages->execute(array(":description" => "$description", ":accelnumber" => "$accelnumber", ":categoryid" => "$categoryidresult", ":imgpath" = > "$imgpath"));
+						$conn = null;
+					}catch(PDOException $e){
+						//print error details to screen
+						echo $result . "<br>" . $e->getMessage();
+						//close database connection
+						$conn = null;
+					}
 					$_SESSION['uploadsuccess']=true;
 					header("Location: ../../adminupload.html");
 					exit();
