8301da2813b19235993c4efc0924942c91ba22d6
ZacharyM <Maggioxg9@aol.com>
2017-01-19 13:51:03 -0600

    update

--- res/functions/adminupload.php
+++ res/functions/adminupload.php
@@ -74,7 +74,7 @@
 					$imgpath = "res/uploads/categories/" . $categoryname . "/" . basename($_FILES["fileToUpload"]["name"]);
 					chmod($target_file, 0777);
 					$img = new Imagick($target_file);
-					img->resizeImage(300,300,Imagick::FILTER_LANCZOS,1,TRUE);
+					$img->resizeImage(300,300,Imagick::FILTER_LANCZOS,1,TRUE);
 					$img->writeImage($imgpath . "resize.jpg");
 					try{
 						$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
