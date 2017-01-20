20f6540bdd8729caf042c7b16765beac0789db43
AccelEntertainment <zacharym@accelentertainment.com>
2017-01-20 00:36:15 -0600

    update

--- res/functions/phpinfo.php
+++ res/functions/phpinfo.php
@@ -1,6 +1,6 @@
 <?php
-if( class_exists("Imagick") )
-{
-    echo "Imagick is installed";
-}
+if (!extension_loaded('imagick'))
+    echo 'imagick not installed';
+else
+    echo 'imagick installed';
 ?>
