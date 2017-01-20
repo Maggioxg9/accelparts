ede439442387142b49d756f148fd5dcb49344a13
ZacharyM <Maggioxg9@aol.com>
2017-01-20 11:13:18 -0600

    update

--- res/functions/adminupload.php
+++ res/functions/adminupload.php
@@ -76,7 +76,8 @@
 					chmod($target_file, 0777);
 					$img = new Imagick($target_file);
 					$img->resizeImage(300,300,Imagick::FILTER_LANCZOS,1,TRUE);
-					$img->writeImage($thumb);
+					$img->setImageFormat("jpeg");
+					file_put_contents ($thumb, $img);
 					try{
 						$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 						$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
